7c360cc0ccb7914d9086599733b2d2dbc6430f90
Raven Robertson <ravewillow6383@gmail.com>
2019-04-16 13:22:58 -0700

    stepped back a few steps. Back to one picture fully functional. Going to build up from here

--- js/app.js
+++ js/app.js
@@ -1,95 +1,37 @@
 'use strict';
 
-var pictureOneReference = document.getElementById('productPic1');
-var pictureTwoReference = document.getElementById('productPic2');
-var pictureThreeReference = document.getElementById('productPic3');
+var productArray =[];
+var totalClicks = 0;
 
-var imageSection = document.getElementById('product-test');
-
-var currentPic1 = 0;
-var currentPic2 = 2;
-var currentPic3 = 3;
-
-var clickTracker = 0;
-var productsArray = [];
-
-function BusMallProduct(picturePath, description) {
-  this.picturePath = picturePath;
-  this.description = description;
-  productsArray.push(this);
-}
-
-BusMallProduct.prototype.renderProductPicture = function() {
-  pictureOneReference.description = this.description;
-  pictureTwoReference.description = this.description;
-  pictureThreeReference.description = this.description;
-};
-
-function getRandomPictureNumber() {
-  var randomNumber = Math.floor(Math.random() * productsArray.length);
+function getRandomPictureNumber(){
+  var randomNumber = Math.floor(Math.random() * productArray.length);
   return randomNumber;
 }
 
-var productDisplay = function (event) {
-  if(event.target.id === 'productPic1' || event.target.id === 'productPic2' || event.target.id ==='productPic3') {
-    do {
-      var randomPic1 = getRandomPictureNumber();
-    } while (randomPic1 === currentPic1 || randomPic1 === currentPic2 || randomPic1 === currentPic3 || randomPic1 === randomPic2 || randomPic1 === randomPic3);
-    do {
-      var randomPic2 = getRandomPictureNumber();
-    } while (randomPic2 === currentPic2 || randomPic2 === currentPic1 || randomPic3 === currentPic3 || randomPic2 === randomPic1 || randomPic2 === randomPic3);
-    do {
-      var randomPic3 = getRandomPictureNumber();
-    } while (randomPic3 === currentPic3|| randomPic3 === currentPic1 || randomPic3 === currentPic2 || randomPic3 === randomPic1 || randomPic3 === randomPic2);
+var previousIndex = -1;
+
+function renderRandomProduct() {
+  var productPictureReference = document.getElementById('productPic1');
+
+  var randomIndex = getRandomPictureNumber();
+
+  while(randomIndex === previousIndex) {
+    randomIndex = getRandomPictureNumber();
   }
-  currentPic1 = randomPic1;
-  currentPic2 = randomPic2;
-  currentPic3 = randomPic3;
+ 
+  previousIndex = randomIndex;
+  var randomProduct = productArray[randomIndex];
 
-  pictureOneReference.description = productsArray[randomPic1].description;
-  pictureTwoReference.description = productsArray[randomPic2].description;
-  pictureThreeReference.description = productsArray[randomPic3].description;
+  productPictureReference.src = randomProduct.picturePath;
+  productPictureReference.alt = randomProduct.description;
+}
 
-  function renderProductPicture1() {
-    var productPictureReference = document.getElementById('ProductPic1');
-    var randomIndex = randomPic1;
-    var randomProduct = productsArray[randomIndex];
-
-    productPictureReference.description = randomProduct.picturePath;
-    productPictureReference.alt = randomProduct.description;
-  }
-  function renderProductPicture2() {
-    var productPictureReference = document.getElementById('ProductPic2');
-    var randomIndex = randomPic2;
-    var randomProduct = productsArray[randomIndex];
-
-    productPictureReference.description = randomProduct.picturePath;
-    productPictureReference.alt = randomProduct.description;
-  }
-  function renderProductPicture3() {
-    var productPictureReference = document.getElementById('ProductPic2');
-    var randomIndex = randomPic3();
-    var randomProduct = productsArray[randomIndex];
-
-    productPictureReference.description = randomProduct.picturePath;
-    productPictureReference.alt = randomProduct.description;
-  }
-
-  renderProductPicture1();
-  renderProductPicture2();
-  renderProductPicture3();
-
-  currentPic1.addEventListener('click', productDisplay);
-  currentPic2.addEventListener('click', productDisplay);
-  currentPic3.addEventListener('click', productDisplay);
-
-  clickTracker++;
-  if(clickTracker === 25) {
-    imageSection.removeEventListener('click', productDisplay);
-  }
-};
-
-imageSection.addEventListener('click', productDisplay);
+function BusMallProduct(picturePath, description){
+  this.picturePath = picturePath;
+  this.description = description;
+  this.timesClicked = 0;
+  productArray.push(this);
+}
 
 new BusMallProduct('img/bag.jpg', 'starwars luggage');
 new BusMallProduct('img/banana.jpg', 'banana slicer');
@@ -112,5 +54,7 @@ new BusMallProduct('img/usb.gif', 'tentacle drive');
 new BusMallProduct('img/water-can.jpg', 'watering can');
 new BusMallProduct('img/wine-glass.jpg', 'spill-safe wine glass');
 
-productDisplay();
+renderRandomProduct();
 
+var productPictureReference = document.getElementById('productPic1');
+productPictureReference.addEventListener('click', renderRandomProduct);
